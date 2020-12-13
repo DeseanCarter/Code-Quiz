74a73d06e27b79ddf407443a4a82428a95fcfc71
Carter <deseancarter1990@gmail.com>
2020-12-13 17:26:55 -0500

    question objects added

---
+++ assets/js/questions.js
@@ -0,0 +1,29 @@
+//Questions for Code Quiz
+
+var questions = [
+    {
+       title: "Commonly used data types DO NOT include:",
+       choices: ["stings", "booleans", "alerts", "numbers"],
+       answers: "alerts"
+    },
+    {
+        title: "The condition in an if / else statement is enclosed within _____.",
+        choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+        answer: "parentheses"
+    },
+    {
+        title: "Arrays in JavaScript can be used to store _____.",
+        choice: ["numbers and strings", "other arrays", "booleans", "all of the above"],
+        answer: "all of the above"
+    },
+    {
+        title: "String values must be enclosed within _____ when being assigned to variables.",
+        choice: ["commas", "curly brackets", "quotes", "parentheses"],
+        answer: "quotes"
+    },
+    {
+        title: "A very useful tool used in development and debugging for printing content to the debugger is:",
+        choice: ["JavaScript", "terminal/bash", "for loops", "console log"],
+        answer: "console log"
+    },
+]
